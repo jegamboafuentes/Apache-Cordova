6bed4998bfe28ecca10d20165b4a35d2b009fb76
Jorge Enrique Gamboa Fuentes <jegamboafuentes@gmail.com>
2016-11-09 00:12:56 -0500

    Updating changes

---
+++ hello/platforms/ios/www/js/myjs.js
@@ -0,0 +1,9 @@
+//window.addEventListener("batterystatus", onBatteryStatus, false);
+//function onBatteryStatus(status) {
+//    alert("hola");
+//    console.log("Level: " + status.level + " isPlugged: " + status.isPlugged);
+//}
+
+myFunction(){
+    alert("hola");
+}
